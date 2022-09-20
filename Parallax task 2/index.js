435ea16a3743a22918fda7a0167a2a03a5fb097a
RuneKodehode <rune.kodehode@gmail.com>
2022-09-20 12:46:15 +0200

    final edit

--- Parallax task 2/index.js
+++ Parallax task 2/index.js
@@ -4,9 +4,11 @@ function heightenPic() {
   if (!scaled) {
     bilde.style.height = "100%";
     scaled = 1;
+    bilde.style.transition = "5s";
     document.getElementById("fsButton").textContent =
       "why would you click this.";
   } else {
+    bilde.style.transition = "3s";
     bilde.style.height = "20%";
     scaled = 0;
     document.getElementById("fsButton").textContent = "stop clicking me.";
