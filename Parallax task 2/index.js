dcd04a31230caa147b550c8b5c9b249ae6fdf68c
RuneKodehode <rune.kodehode@gmail.com>
2022-09-20 10:55:48 +0200

    .

--- Parallax task 2/index.js
+++ Parallax task 2/index.js
@@ -4,10 +4,11 @@ function heightenPic() {
   if (!scaled) {
     bilde.style.height = "100%";
     scaled = 1;
-    document.getElementById("fsButton").textContent = "why would you click this."
+    document.getElementById("fsButton").textContent =
+      "why would you click this.";
   } else {
-    bilde.style.height = "40%";
+    bilde.style.height = "20%";
     scaled = 0;
-    document.getElementById("fsButton").textContent = "stop clicking me."
+    document.getElementById("fsButton").textContent = "stop clicking me.";
   }
 }
