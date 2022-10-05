7f8929131a33d5bbc59a486e89cab44c763b0307
RuneKodehode <rune.kodehode@gmail.com>
2022-10-05 13:36:41 +0200

    asdasd

--- piano/index.js
+++ piano/index.js
@@ -1,61 +1,53 @@
-for (let i = 0; i < 7; i++) {}
-
-function cBtn() {
-  new Audio("notes/C5.mp3").play();
-  ckey.style.boxShadow = "-4px -4px 4px black";
-  setTimeout(() => {
-    ckey.style.boxShadow = "0px 0px 0px black";
-  }, 100);
-}
-function dBtn() {
-  new Audio("notes/D5.mp3").play();
-  dkey.style.boxShadow = "-4px -4px 4px black";
-  setTimeout(() => {
-    dkey.style.boxShadow = "0px 0px 0px black";
-  }, 100);
-}
-function eBtn() {
-  new Audio("notes/E5.mp3").play();
-  ekey.style.boxShadow = "-4px -4px 4px black";
-  setTimeout(() => {
-    ekey.style.boxShadow = "0px 0px 0px black";
-  }, 100);
-}
-function fBtn() {
-  new Audio("notes/F5.mp3").play();
-  fkey.style.boxShadow = "-4px -4px 4px black";
-  setTimeout(() => {
-    fkey.style.boxShadow = "0px 0px 0px black";
-  }, 100);
-}
-function gBtn() {
-  new Audio("notes/G5.mp3").play();
-  gkey.style.boxShadow = "-4px -4px 4px black";
-  setTimeout(() => {
-    gkey.style.boxShadow = "0px 0px 0px black";
-  }, 100);
-}
-function aBtn() {
-  new Audio("notes/A5.mp3").play();
-  akey.style.boxShadow = "-4px -4px 4px black";
-  setTimeout(() => {
-    akey.style.boxShadow = "0px 0px 0px black";
-  }, 100);
-}
-function bBtn() {
-  new Audio("notes/B5.mp3").play();
-  bkey.style.boxShadow = "-4px -4px 4px black";
-  setTimeout(() => {
-    bkey.style.boxShadow = "0px 0px 0px black";
-  }, 100);
-}
+const btn = document.querySelectorAll("button");
 
 window.addEventListener("keydown", (event) => {
-  if (event.key === "a") cBtn();
-  if (event.key === "s") dBtn();
-  if (event.key === "d") eBtn();
-  if (event.key === "f") fBtn();
-  if (event.key === "g") gBtn();
-  if (event.key === "h") aBtn();
-  if (event.key === "j") bBtn();
+  if (event.key === "a") {
+    new Audio("notes/C5.mp3").play();
+    btn[0].style.boxShadow = "0px 0px 7px black";
+    setTimeout(() => {
+      btn[0].style.boxShadow = "0px 0px 0px black";
+    }, 100);
+  }
+  if (event.key === "s") {
+    new Audio("notes/D5.mp3").play();
+    btn[1].style.boxShadow = "0px 0px 7px black";
+    setTimeout(() => {
+      btn[1].style.boxShadow = "0px 0px 0px black";
+    }, 100);
+  }
+  if (event.key === "d") {
+    new Audio("notes/E5.mp3").play();
+    btn[2].style.boxShadow = "0px 0px 7px black";
+    setTimeout(() => {
+      btn[2].style.boxShadow = "0px 0px 0px black";
+    }, 100);
+  }
+  if (event.key === "f") {
+    new Audio("notes/F5.mp3").play();
+    btn[3].style.boxShadow = "0px 0px 7px black";
+    setTimeout(() => {
+      btn[3].style.boxShadow = "0px 0px 0px black";
+    }, 100);
+  }
+  if (event.key === "g") {
+    new Audio("notes/G5.mp3").play();
+    btn[4].style.boxShadow = "0px 0px 7px black";
+    setTimeout(() => {
+      btn[4].style.boxShadow = "0px 0px 0px black";
+    }, 100);
+  }
+  if (event.key === "h") {
+    new Audio("notes/A5.mp3").play();
+    btn[5].style.boxShadow = "0px 0px 7px black";
+    setTimeout(() => {
+      btn[5].style.boxShadow = "0px 0px 0px black";
+    }, 100);
+  }
+  if (event.key === "j") {
+    new Audio("notes/B5.mp3").play();
+    btn[6].style.boxShadow = "0px 0px 7px black";
+    setTimeout(() => {
+      btn[6].style.boxShadow = "0px 0px 0px black";
+    }, 100);
+  }
 });
